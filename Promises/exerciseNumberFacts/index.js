980307aead78307467dbeb030421c3fbc16d2348
Rohan Gupta <rohan.gupta@gameskraft.com>
2024-01-25 00:43:06 +0530

    Add exercise question details

--- Promises/exerciseNumberFacts/index.js
+++ Promises/exerciseNumberFacts/index.js
@@ -1,3 +1,31 @@
+/*
+
+1. Write a function, showNumberTrivia, that makes a request to the Numbers API
+(http://numbersapi.com) to get trivia about your favorite number.
+(Make sure you get back JSON — you may need to look at the documentation
+of the API to see how to do this: Details.) Log the piece of trivia to the
+console.
+
+2. Have a “race”: make a new function, showNumberRace, that asks for
+trivia about four different numbers (using four separate requests), but,
+as soon as one request returns, log the piece of trivia for the winning
+number to the console.
+
+3. Get all: make a new function, showNumberAll. that asks for trivia about
+different numbers. Make all of the requests at the same time, but handle
+them once all requests are completed.
+
+However, at least one of the “numbers” you use should be an invalid thing,
+like the string “WRONG”.
+
+Log to the console the array of trivia for responses with a successful status code, and the array of error messages for the responses with a failed status code.
+
+4. Write a function, main, which calls all three of those functions, in
+order, moving onto the next function only after the current function fully
+completes.
+
+*/
+
 const NUMBER_API_BASE_URI = 'http://numbersapi.com';
 
 const showNumberTrivia = async (num) => {
