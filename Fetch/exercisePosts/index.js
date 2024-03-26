a11c9f74f78fcb7df27a52b0c76b2941d6cdd1d4
Rohan Gupta <rohan.gupta@gameskraft.com>
2024-03-26 10:31:27 +0530

    Practice posts exercise (create post)

--- Fetch/exercisePosts/index.js
+++ Fetch/exercisePosts/index.js
@@ -2,6 +2,7 @@
   const POSTS_BASE_URI = 'https://jsonplaceholder.typicode.com/posts';
 
   const posts = document.getElementById('posts');
+  const addPostBtn = document.getElementById('add-button');
 
   const getPosts = async () => {
     try {
@@ -9,6 +10,25 @@
       return data;
     } catch (e) {
       return [];
+    }
+  };
+
+  const addPost = async ({ title, body }) => {
+    try {
+      const data = await fetch(POSTS_BASE_URI, {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          body,
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }).then((res) => res.json());
+      return data;
+    } catch (e) {
+      return null;
     }
   };
 
@@ -41,10 +61,33 @@
     posts.appendChild(fragment);
   };
 
+  const appendNewPost = async (data) => {
+    const post = await addPost(data);
+    posts.prepend(createPost(post));
+  };
+
+  const handleAddPostBtnClick = async () => {
+    // collect form data
+    const title = document.querySelector('input[name="title"]');
+    const body = document.getElementById('body');
+
+    // create new post
+    await appendNewPost({
+      title: title.value,
+      body: body.value,
+    });
+
+    // clear form inputs
+    title.value = '';
+    body.value = '';
+  };
+
   const init = async () => {
     const postsData = await getPosts();
     populatePosts(postsData);
   };
 
   init();
+  
+  addPostBtn.addEventListener('click', handleAddPostBtnClick);
 })()
